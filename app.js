722a5be71a00b8d9cce9cb1a05cc578fd39686b1
Allyson Torres <allysonnamikase@gmail.com>
2023-12-29 20:07:05 -0300

    Desafios 02

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 alert("Boas Vindas ao jogo do numero secreto");
 let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 e 10");
+console.log(numeroSecreto);
+let chute;
 
-if(numeroSecreto == chute) {
-    console.log("Isso ai! Você descobriu o número secreto (5)");
+// enquanto
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 e 10");
+    // se chute for igual a numero secreto
+    if(numeroSecreto == chute) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    } else {
+        if(chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
 }
