cdc100b93cad77dffdeda7d404418bb073be1f3e
Allyson Torres <allysonnamikase@gmail.com>
2024-01-08 15:14:46 -0300

    Curoso de logica

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ alert("Boas Vindas ao jogo do numero secreto");
 let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute;
+let tentativas = 1;
 
 // enquanto
 while (chute != numeroSecreto) {
     chute = prompt("Escolha um número entre 1 e 10");
     // se chute for igual a numero secreto
     if(numeroSecreto == chute) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if(chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -16,4 +17,6 @@ while (chute != numeroSecreto) {
             alert(`O número secreto é maior que ${chute}`);
         }
     }
+    // tentativas = tentativas + 1;
+    tentativas++;
 }
