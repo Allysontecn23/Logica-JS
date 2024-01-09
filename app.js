b8b2ea83a9156ec3e8a700751fca8c60397b6c5f
Allyson Torres <allysonnamikase@gmail.com>
2024-01-09 18:50:13 -0300

    Conclusão de curso

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 alert("Boas Vindas ao jogo do numero secreto");
-let numeroSecreto = 5;
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto
 while (chute != numeroSecreto) {
-    chute = prompt("Escolha um número entre 1 e 10");
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual a numero secreto
     if(numeroSecreto == chute) {
         break;
@@ -21,8 +22,12 @@ while (chute != numeroSecreto) {
     tentativas++;
 }
 
-if (tentativas > 1) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-} else {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-}
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
